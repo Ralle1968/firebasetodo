390cbe7c9c2c1e08aef1c6b6ce2d31812c7e0db1
Ralle1968 <graef.ralf@gmail.com>
2018-02-20 16:24:40 +0100

    Footer geändert

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,6 @@
     <div class="footer-copyright">
       <div class="container">
       © 2018 Ralf Gräf
-      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
       </div>
     </div>
   </footer>
