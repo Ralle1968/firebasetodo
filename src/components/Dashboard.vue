16df4cdea4199e6a573acf002f5f5f333303aa3a
Ralle1968 <graef.ralf@gmail.com>
2018-02-27 23:12:14 +0100

    add selected dashboards

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="dashboard" class="">
     <ul class="collection with-header col s6">
-      <li class="collection-header"><h4>Todos</h4></li>
+      <li class="collection-header"><h4 class="flow-text">Zu erledigen: <span class="right flow-text"><small>{{selected}}</small></span></h4></li>
       <li v-for="todo in todos" 
       v-bind:key="todo.id" 
       class="collection-item">
@@ -17,15 +17,14 @@
       </p>
       </li>
     </ul>
-   <div id="sidebar" class="col s3 offset-s2">
-  <label>Bitte Kategorie wählen</label>
+   <div id="sidebar" class="col s5 offset-s1">
+     <br>
     <select class="browser-default" v-model="selected">
-      <option value="" disabled selected>Kategorie wählen</option>
+      <option value="Alle Kategorien">Alle Kategorien</option>
       <option v-for="todo in todos" 
       v-bind:key="todo.id">{{todo.todo_category}}</option>
     </select>
-  <p>Gewählt: {{selected}}</p>
-</div>
+    </div>
   </div>
 </template>
 
@@ -33,19 +32,28 @@
 import db from './firebaseInit'
   export default {
     name: 'dashboard',
+
     data (){
       return {
         todos:[],
         id: null ,
         todo_checked: null,
-        selected:''
+        selected:'Alle Kategorien'
       }
     },
+
     created(){
-      this.fetchData()
+      this.fetchAllData()
     },
+
+    watch: {
+      selected: function() {
+        this.fetchSelectedData(this.selected)
+      }
+    },
+
     methods:{
-      fetchData(){
+      fetchAllData(){
         db.collection('todos').orderBy('todo_time').get().then(querySnapshot =>{
           querySnapshot.forEach(doc =>{
             const data = {
@@ -61,6 +69,29 @@ import db from './firebaseInit'
         })
       },
 
+      fetchSelectedData(sel){
+        alert(sel)
+        if (sel=='Alle Kategorien') {
+          this.todos = []
+          this.fetchAllData()
+        } else {
+          db.collection('todos').where('todo_category', '==', sel).get().then(querySnapshot =>{
+            querySnapshot.forEach(doc =>{
+              const data = {
+                'id': doc.id,
+                'todo_id': doc.data().todo_id,
+                'todo_category': doc.data().todo_category,
+                'todo_checked': doc.data().todo_checked,
+                'todo_title': doc.data().todo_title,
+                'todo_time': doc.data().todo_time.toLocaleString('de-DE')
+              }
+              this.todos = []
+              this.todos.push(data)
+            })
+          })
+        }
+      },
+
       updateStatus(ID, sta){
         console.log(ID)
         console.log(sta)        
@@ -70,7 +101,7 @@ import db from './firebaseInit'
         .then(()=>{
           alert("Success!!")
           this.todos = []
-          this.fetchData()
+          this.fetchAllData()
         
         })
       } 
